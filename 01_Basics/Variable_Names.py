f5ba558b91e08426e6d110a9abe7c658228bb35c
ALCOR419 <ashishmore1235@gmail.com>
2026-09-11 16:45:35 +0530

    Variable names with more than one word can be difficult to read

---
+++ 01_Basics/Variable_Names.py
@@ -0,0 +1,10 @@
+#Camel Case
+myVariableName = "Ashish"
+
+#Pascal Case
+MyVariableName = "Subhash"
+
+#Snake Case
+my_variable_name = "More"
+
+print(myVariableName , MyVariableName , my_variable_name) 
